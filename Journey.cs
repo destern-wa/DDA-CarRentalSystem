74591749ce16beff0a0c95b209541b93b0a12ce9
David <20023188@tafe.wa.edu.au>
2019-10-17 10:01:16 +0800

    Converted from Java to C-sharp

---
+++ Journey.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp_Assignment2
+{
+    public class Journey
+    {
+        private double kilometers;
+
+        /**
+         * Class constructor
+         */
+        public Journey()
+        {
+            this.kilometers = 0;
+        }
+
+        /** 
+         * Appends the distance parameter to {@link #kilometers}
+         * @param kilometers the distance traveled 
+         */
+        public void addKilometers(double kilometers)
+        {
+            this.kilometers += kilometers;
+        }
+
+
+
+        /**
+         * Getter method for total Kilometers traveled in this journey.
+         * @return {@link #kilometers}
+         */
+        public double getKilometers()
+        {
+            return kilometers;
+        }
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp_Assignment2
+{
+    public class Vehicle
+    {
+
+        private string manufacturer;
+        private string model;
+        private int makeYear;
+        // TODO add Registration Number 
+        // TODO add variable for OdometerReading (in KM), 
+        // TODO add variable for TankCapacity (in litres)
+
+        private FuelPurchase fuelPurchase;
+
+        /**
+         * Class constructor specifying name of make (manufacturer), model and year
+         * of make.
+         * @param manufacturer
+         * @param model
+         * @param makeYear
+         */
+        public Vehicle(string manufacturer, string model, int makeYear)
+        {
+            this.manufacturer = manufacturer;
+            this.model = model;
+            this.makeYear = makeYear;
+            fuelPurchase = new FuelPurchase();
+        }
+
+        // TODO Add missing getter and setter methods
+
+        /**
+         * Prints details for {@link Vehicle}
+         */
+        public void printDetails()
+        {
+            Console.WriteLine("Vehicle: " + makeYear + " " + manufacturer + " " + model);
+            // TODO Display additional information about this vehicle
+        }
+
+
+        // TODO Create an addKilometers method which takes a parameter for distance travelled 
+        // and adds it to the odometer reading. 
+
+        // adds fuel to the car
+        public void addFuel(double litres, double price)
+        {
+            fuelPurchase.purchaseFuel(litres, price);
+        }
+    }
+}
