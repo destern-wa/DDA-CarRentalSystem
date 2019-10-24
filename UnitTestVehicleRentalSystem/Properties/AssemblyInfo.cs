f30a1b69f7efdf41aa6ea277b5d358c4c367b889
David <20023188@tafe.wa.edu.au>
2019-10-24 09:38:39 +0800

    Created unit tests

---
+++ UnitTestVehicleRentalSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestVehicleRentalSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("North Metro TAFE")]
+[assembly: AssemblyProduct("UnitTestVehicleRentalSystem")]
+[assembly: AssemblyCopyright("Copyright © North Metro TAFE 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f96cf3c-99ed-4276-bbdf-83c0d6c21dde")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
