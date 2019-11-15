f74b4ad264d654c28b7f7158d9ec5776cf682281
David <20023188@tafe.wa.edu.au>
2019-11-15 11:53:12 +0800

    Move ViewModelBase code back into its own file

--- VehicleRentalSystem/Message.cs
+++ VehicleRentalSystem/Message.cs
@@ -16,39 +16,6 @@ namespace VehicleRentalSystem
     {
         void Handle(T obj);
     }
-    public abstract class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected bool SetProperty<T>(ref T field, T newValue,
-        [CallerMemberName]string propertyName = null)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                field = newValue;
-                PropertyChanged?.Invoke(this,
-                new PropertyChangedEventArgs(propertyName));
-                return true;
-            }
-            return false;
-        }
-    }
-    public abstract class ViewModelBase<T> : IListen<T>
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected bool SetProperty<T>(ref T field, T newValue,
-        [CallerMemberName]string propertyName = null)
-        {
-            if (!EqualityComparer<T>.Default.Equals(field, newValue))
-            {
-                field = newValue;
-                PropertyChanged?.Invoke(this,
-                new PropertyChangedEventArgs(propertyName));
-                return true;
-            }
-            return false;
-        }
-        public abstract void Handle(T obj);
-    }
 
     public class EventAggregator
     {
--- VehicleRentalSystem/ViewModelBase.cs
+++ VehicleRentalSystem/ViewModelBase.cs
@@ -4,20 +4,37 @@ using System.Runtime.CompilerServices;
 
 namespace VehicleRentalSystem
 {
-    //public abstract class ViewModelBase : IListen
-    //{
-    //    public event PropertyChangedEventHandler PropertyChanged;
-    //    protected bool SetProperty<T>(ref T field, T newValue,
-    //    [CallerMemberName]string propertyName = null)
-    //    {
-    //        if (!EqualityComparer<T>.Default.Equals(field, newValue))
-    //        {
-    //            field = newValue;
-    //            PropertyChanged?.Invoke(this,
-    //            new PropertyChangedEventArgs(propertyName));
-    //            return true;
-    //        }
-    //        return false;
-    //    }
-    //}
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue,
+        [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+    }
+    public abstract class ViewModelBase<T> : IListen<T>
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue,
+        [CallerMemberName]string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+        public abstract void Handle(T obj);
+    }
 }
