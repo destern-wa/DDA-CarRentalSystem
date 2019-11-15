4c0bf70d877286da0f79eab34ddd19c7149caf88
David <20023188@tafe.wa.edu.au>
2019-11-15 11:50:06 +0800

    More specific validation, error message display for Return vehicle view/viewmodel

--- VehicleRentalSystem/ViewModel/ReturnVehicleViewModel.cs
+++ VehicleRentalSystem/ViewModel/ReturnVehicleViewModel.cs
@@ -35,6 +35,33 @@ namespace VehicleRentalSystem.ViewModel
                 SetProperty(ref _errorMessage, value);
             }
         }
+        private string _errorMessageDate;
+        public string ErrorMessageDate
+        {
+            get => _errorMessageDate;
+            set
+            {
+                SetProperty(ref _errorMessageDate, value);
+            }
+        }
+        private string _errorMessageKm;
+        public string ErrorMessageKm
+        {
+            get => _errorMessageKm;
+            set
+            {
+                SetProperty(ref _errorMessageKm, value);
+            }
+        }
+        private string _errorMessageFuel;
+        public string ErrorMessageFuel
+        {
+            get => _errorMessageFuel;
+            set
+            {
+                SetProperty(ref _errorMessageFuel, value);
+            }
+        }
 
         private DateTime _returnDate;
         public DateTime ReturnDate
@@ -108,8 +135,43 @@ namespace VehicleRentalSystem.ViewModel
             get => _cancelCommand;
         }
 
+        private bool validate()
+        {
+            ErrorMessage = "";
+
+            // Validate return date
+            ErrorMessageDate = ReturnDate > DateTime.Now ? "Return date can not be in the future" : "";
+
+            // Validate km travelled
+            double km = -1;
+            ErrorMessageKm = String.IsNullOrWhiteSpace(KmTravelled)
+                ? "Distance is required"
+                : (double.TryParse(KmTravelled, out km) ? "" : "Distance must be numeric");
+            if (ErrorMessageKm == "")
+            {
+                ErrorMessageKm = km <= 0 ? "Distance must be a positive number" : "";
+            }
+
+            // Validate fuel
+            double fuel = -1;
+            double cost = -1;
+            ErrorMessageFuel = String.IsNullOrWhiteSpace(FuelAmount) || String.IsNullOrWhiteSpace(FuelCost)
+                ? "Fuel quantity and cost are required"
+                : ( double.TryParse(FuelAmount, out fuel) && double.TryParse(FuelCost, out cost)
+                    ? "" : "Values must be numeric" );
+            if (ErrorMessageFuel == "")
+            {
+                ErrorMessageFuel = fuel <= 0 || cost < 0 ? "Amounts must be positive numbers" : "";
+            }
+
+            return ErrorMessageDate == "" && ErrorMessageKm == "" && ErrorMessageFuel == "";
+
+        }
+
         private bool SaveReturn()
         {
+            bool isValid = validate();
+            if (!isValid) return false;
             try
             {
                 double km = double.Parse(KmTravelled);
